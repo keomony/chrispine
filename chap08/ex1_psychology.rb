ac81230d75346c089ca55feba5834fb748b327b6
Keomony Khun <keomony@gmail.com>
2016-10-24 16:37:16 +0100

    refactoring the program

--- chap08/ex1_psychology.rb
+++ chap08/ex1_psychology.rb
@@ -1,65 +1,41 @@
-goodAnswer = false
+# let’s try that psychology experiment program again.
+# This time we’ll write a method to ask the questions for us.
+# It will need to take the question as a parameter and return true if the
+# person answers yes and false if they answer no. (Even though most of the time
+# we just ignore the answer, it’s still a good idea for our method to return the
+# answer. This way we can use it for the bed-wetting question, too.) I’m also
+# going to shorten the greeting and the debriefing, just so this is easier to read:
 
-# while (not 'false') = true ==> while true, keeps looping
-#until (not 'true') = false stop the loop
-while (not goodAnswer)
-  puts 'Do you like eating tacos?'
-  answer = gets.chomp.downcase
-  if (answer == 'yes' or answer == 'no')
-    goodAnswer = true
-  else
-    puts 'Please answer "yes" or "no".'
-  end
-end
-
-goodAnswer = false
-while (not goodAnswer)
-  puts 'Do you like eating burritos?'
-  answer = gets.chomp.downcase
-  if (answer == 'yes' or answer == 'no')
-    goodAnswer = true
-  else
-    puts 'Please answer "yes" or "no".'
-  end
-end
-
-# We pay attention to *this* answer, though.
-goodAnswer = false
-while (not goodAnswer)
-  puts 'Do you wet the bed?'
-  answer = gets.chomp.downcase
-  if (answer == 'yes' or answer == 'no')
-    goodAnswer = true
-    if answer == 'yes'
-      wetsBed = true
+def ask question
+  while true
+    puts question
+    reply = gets.chomp.downcase
+    if (reply == 'yes' || reply == 'no')
+      if reply == 'yes'
+        answer = true
+      else
+        answer = false
+      end
+      break
     else
-      wetsBed = false
+      puts 'Please answer "yes" or "no".'
     end
-  else
-    puts 'Please answer "yes" or "no".'
   end
+  answer # This is what we return (true or false).
 end
 
-goodAnswer = false
-while (not goodAnswer)
-  puts 'Do you like eating chimichangas?'
-  answer = gets.chomp.downcase
-  if (answer == 'yes' or answer == 'no')
-    goodAnswer = true
-  else
-    puts 'Please answer "yes" or "no".'
-  end
-end
-
+puts 'Hello, and thank you for...'
+puts
+ask 'Do you like eating tacos?' # Ignore this return value
+ask 'Do you like eating burritos?' # And this one
+wets_bed = ask 'Do you wet the bed?' # Save this return value
+ask 'Do you like eating chimichangas?'
+ask 'Do you like eating sopapillas?'
 puts 'Just a few more questions...'
-
-goodAnswer = false
-while (not goodAnswer)
-  puts 'Do you like eating sopapillas?'
-  answer = gets.chomp.downcase
-  if (answer == 'yes' or answer == 'no')
-    goodAnswer = true
-  else
-    puts 'Please answer "yes" or "no".'
-  end
-end
+ask 'Do you like drinking horchata?'
+ask 'Do you like eating flautas?'
+puts
+puts 'DEBRIEFING:'
+puts 'Thank you for...'
+puts
+puts wets_bed
