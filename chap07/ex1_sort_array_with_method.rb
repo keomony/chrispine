c66df9b42ca15bd1c437e339003f1f109d41f558
Keomony Khun <keomony@gmail.com>
2016-10-24 18:52:51 +0100

    wrap it with a method

--- chap07/ex1_sort_array_with_method.rb
+++ chap07/ex1_sort_array_with_method.rb
@@ -1,4 +1,8 @@
 #one word per line, continuing until we just press Enter on an empty line
+def sort_array array
+  #array.sort_by ignores case
+  array.sort_by{|w| w.downcase}.join(" ")
+end
 
 words_in_array = []
 puts "Please enter a word "
@@ -8,5 +12,5 @@ begin
   #add to the array only if user types something
   words_in_array.push input_word if input_word != ''
 end while input_word != ''
-#array.sort_by ignores case
-puts words_in_array.sort_by{|w| w.downcase}.join(" ")
+
+puts sort_array words_in_array
